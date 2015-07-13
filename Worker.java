3aca6f72ddeffb015970db89e450ed7b32cad445
kullen <kullen@auburn.edu>
2015-07-13 14:39:42 -0500

    add new employee

--- Worker.java
+++ Worker.java
@@ -9,8 +9,8 @@ public static Scanner scan = new Scanner(System.in);
 	
 	public static void newWorker(Connection con) throws SQLException{
 		System.out.println("New Worker Entry");
-		String first,last,ssn;
-		int globalID, jobID;
+		String first,last,ssn,select;
+		int globalID, jobID,x;
 
 		System.out.print("First Name: ");
 		first = scan.next();
@@ -21,15 +21,19 @@ public static Scanner scan = new Scanner(System.in);
 		System.out.print("SSN: ");
 		ssn = scan.next();
 
-		System.out.print("Select Job Title: ");
+		System.out.println("Select Job Title: ");
 
-		String[] id = {"title"};
+		String[] id = {"title","jobID"};
 		String[][] titles = TableMethods.selectXFrom("jobTitle",id, con);
 
 		for(int i = 0; i < titles.length; i++){
-			System.out.println(titles[i][0]);
+			System.out.println(" " + (i+1) + ": " + titles[i][0]);
 		}
-
+		System.out.print("Enter: ");
+		select = scan.next();
+		x = Integer.parseInt(select);
+		jobID = Integer.parseInt(titles[x-1][1]);
+		System.out.println(jobID + "");
 	}
 
 
